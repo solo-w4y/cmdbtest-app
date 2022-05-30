343d78f434364309c69f6f2b4da0e08327135699
solo-w4y <solo.arti@mail.ru>
2022-05-30 06:11:31 +0300

    Initial commit

---
+++ public/js/about.0ff69104.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcmdbtest_vue"]=self["webpackChunkcmdbtest_vue"]||[]).push([[443],{7381:function(t,e,n){n.r(e),n.d(e,{default:function(){return d}});var u=n(3396);const s={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),c=[a];function r(t,e){return(0,u.wg)(),(0,u.iD)("div",s,c)}var i=n(89);const o={},b=(0,i.Z)(o,[["render",r]]);var d=b}}]);
+//# sourceMappingURL=about.0ff69104.js.map
